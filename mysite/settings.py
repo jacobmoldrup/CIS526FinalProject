049d8149f7ee72d2ff68134386ab5b6ad3c12f0b
jtmoldrup <jtmoldrup@ksu.edu>
2017-04-08 16:06:43 -0500

    Added localhost to allowed hosts.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't)*1+&3dh_+uwm(+mb_c^7622)5#s$%@3yz96&*xwi1i70!@rp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'cslinux.cs.ksu.edu']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
