abd72abd183d2fc788513e7c1c99a381b7efe0e0
jtmoldrup <jtmoldrup@ksu.edu>
2017-04-13 13:11:08 -0500

    put class in quotes in ForeignKey.

--- BikeShop/models.py
+++ BikeShop/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
+# Create your models here.
 
 class Cities(models.Model):
     name = models.TextField(max_length=40)
     state = models.TextField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name + ' ' + self.state
+
     class Meta:
         unique_together = ('name', 'state')
 
@@ -14,38 +18,37 @@ class Cities(models.Model):
 class BikeShops(models.Model):
     name = models.TextField(max_length=40)
     address = models.TextField(max_length=50)
-    city_id = models.ForeignKey(Cities, on_delete=models.CASCADE)
+    city_id = models.ForeignKey('Cities', on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('city_id', 'address', 'name')
 
 
 class Companies(models.Model):
-    code = models.IntegerField(primary_key = True)
+    code = models.IntegerField(primary_key=True)
     name = models.TextField(max_length=40)
     address = models.TextField(max_length=50)
-    city_id = models.ForeignKey(Cities, on_delete=models.CASCADE)
-    
+    city_id = models.ForeignKey('Cities', on_delete=models.CASCADE)
+
     class Meta:
         unique_together = ('name', 'address', 'city_id')
 
 
 class Bikes(models.Model):
     type = models.TextField(max_length=40)
-    company_code = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    company_code = models.ForeignKey('Companies', on_delete=models.CASCADE)
     model = models.TextField(max_length=40)
     year = models.IntegerField
 
 
-
 class Companies_Bikes(models.Model):
-    company_code = models.ForeignKey(Companies, primary_key=True)
-    bike_id = models.ForeignKey(Bikes, primary_key=True)
+    company_code = models.ForeignKey('Companies', primary_key=True)
+    bike_id = models.ForeignKey('Bikes', primary_key=True)
 
 
 class BikeShops_Bikes(models.Model):
-    bikeshop_id = models.ForeignKey(BikeShops, primary_key=True)
-    bike_id = models.ForeignKey(Bikes, primary_key=True)
+    bikeshop_id = models.ForeignKey('BikeShops', primary_key=True)
+    bike_id = models.ForeignKey('Bikes', primary_key=True)
     price = models.FloatField()
 
 
@@ -53,42 +56,42 @@ class Services(models.Model):
     price = models.FloatField()
     estimated_finish = models.DateTimeField()
     description = models.TextField(max_length=100)
-    bike_id = models.ForeignKey(Bikes)
+    bike_id = models.ForeignKey('Bikes')
     scheduled_start_date = models.DateTimeField()
-    
+
     class Meta:
         unique_together = ('bike_id', 'scheduled_start_date')
 
 
 class Products(models.Model):
     name = models.TextField(max_length=40)
-    company_code = models.ForeignKey(Companies)
+    company_code = models.ForeignKey('Companies')
     description = models.TextField(max_length=100)
 
 
 class BikeShops_Products(models.Model):
-    bikeshop_id = models.ForeignKey(BikeShops, primary_key=True)
-    product_id = models.ForeignKey(Products, primary_key=True)
+    bikeshop_id = models.ForeignKey('BikeShops', primary_key=True)
+    product_id = models.ForeignKey('Products', primary_key=True)
     price = models.FloatField()
 
 
 class Companies_Products(models.Model):
-    company_id = models.ForeignKey(Companies, primary_key=True)
-    product_id = models.ForeignKey(Products, primary_key=True)
+    company_id = models.ForeignKey('Companies', primary_key=True)
+    product_id = models.ForeignKey('Products', primary_key=True)
 
 
 class People(models.Model):
     first_name = models.TextField(max_length=40)
     last_name = models.TextField(max_length=40)
     address = models.TextField(max_length=40)
-    city_id = models.ForeignKey(Cities)
+    city_id = models.ForeignKey('Cities')
 
     class Meta:
         abstract = True
 
 
 class Employees(People):
-    bikeshop_id = models.ForeignKey(BikeShops)
+    bikeshop_id = models.ForeignKey('BikeShops')
     title = models.TextField(max_length=40)
     salery = models.FloatField()
 
@@ -101,28 +104,26 @@ class Customer(People):
 
 
 class ShopEmployees(models.Model):
-    person_id = models.ForeignKey(Employees, primary_key=True, on_delete=models.CASCADE)
-    bike_shop_id = models.ForeignKey(BikeShops, primary_key=True, on_delete=models.CASCADE)
+    person_id = models.ForeignKey('Employees', primary_key=True, on_delete=models.CASCADE)
+    bike_shop_id = models.ForeignKey('BikeShops', primary_key=True, on_delete=models.CASCADE)
 
 
 class CustomerServiceRequests(models.Model):
-    person_id = models.ForeignKey(Customer, primary_key=True, on_delete=models.CASCADE)
-    service_id = models.ForeignKey(Services, primary_key=True)
-    bikeshop_id = models.ForeignKey(BikeShops, on_delete=models.CASCADE)
-    bike_id = models.ForeignKey(Bikes, primary_key=True)
+    person_id = models.ForeignKey('Customer', primary_key=True, on_delete=models.CASCADE)
+    service_id = models.ForeignKey('Services', primary_key=True)
+    bikeshop_id = models.ForeignKey('BikeShops', on_delete=models.CASCADE)
+    bike_id = models.ForeignKey('Bikes', primary_key=True)
     time_requested = models.DateTimeField()
 
     class Meta:
         unique_together = ('time_requested', 'bike_id')
 
+
 class BikeShopServices(models.Model):
-    bikeshop_id = models.ForeignKey(BikeShops, primary_key=True, on_delete=models.CASCADE)
-    service_id = models.ForeignKey(Services, primary_key=True)
+    bikeshop_id = models.ForeignKey('BikeShops', primary_key=True, on_delete=models.CASCADE)
+    service_id = models.ForeignKey('Services', primary_key=True)
 
 
 class Customer_Bikes(models.Model):
-    person_id = models.ForeignKey(Customer, primary_key=True, on_delete=models.CASCADE)
-    bike_id = models.ForeignKey(Bikes, primary_key=True, on_delete=models.CASCADE)
-
-
-
+    person_id = models.ForeignKey('Customer', primary_key=True, on_delete=models.CASCADE)
+    bike_id = models.ForeignKey('Bikes', primary_key=True, on_delete=models.CASCADE)
