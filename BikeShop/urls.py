f38f40b68663d0c2277d2b4bada657bef3d84e50
jtmoldrup <jtmoldrup@ksu.edu>
2017-05-04 10:50:52 -0500

    Now viewing shop stuff works.

--- BikeShop/urls.py
+++ BikeShop/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_main_page, name='post_main_page'),
-    url(r'^Bikes$', views.view_all_bikes, name='view_all_bikes'),
-    url(r'^products$', views.view_all_products, name='view_all_products'),
-    url(r'^shops$', views.view_all_shops, name='view_all_shops'),
+    url(r'^view_all_bikes', views.view_all_bikes, name='view_all_bikes'),
+    url(r'^view_all_products', views.view_all_products, name='view_all_products'),
+    url(r'^view_all_shops', views.view_all_shops, name='view_all_shops'),
     url(r'^view_shop_items/(?P<pk>\w+)/$', views.view_shop_items, name='view_shop_items'),
 
     url(r'^request_service$', views.request_service, name='request_service'),
